21ed158f9f14a575780cf75cfc14484535cc59c0
GoyonStephane <stephane.goyon@gmail.com>
2016-05-30 19:09:48 +0200

    #4 add class Character

---
+++ src/Character.php
@@ -0,0 +1,27 @@
+<?php
+namespace Game;
+
+/**
+ * Class Character
+ * @package Game
+ * @author : Stephane Goyon <stephane.goyon@gmail.com>
+ */
+class Character
+{
+    /**
+     * Character constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Return Hello
+     *
+     * @return string
+     */
+    public function sayHello()
+    {
+        return 'Hello';
+    }
+}
